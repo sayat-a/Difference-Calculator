8ed8c8c1ea3161fcd1fa18e808a022d1918b850e
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-15 01:34:59 +0500

    initial commit

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import argparse
+from gendiff.gen_diff import generate_diff
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration file and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+    args = parser.parse_args()
+    print(args.first_file, args.second_file)
+    print(generate_diff(args.first_file, args.second_file))
+
+
+if __name__ == '__main__':
+    main()

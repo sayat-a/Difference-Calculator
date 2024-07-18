7cbf7bf91e7868765f03d26eaa1d8ae1bdedc13b
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-19 01:55:44 +0500

    add tests/test_gen_diff.py, fixtures

---
+++ tests/test_gen_diff.py
@@ -0,0 +1,15 @@
+from gendiff.gen_diff import generate_diff
+
+
+def test_generate_diff():
+    expected_result = """{
+  - follow: False
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: True
+}"""
+    file1_path = 'tests/fixtures/file1.json'
+    file2_path = 'tests/fixtures/file2.json'
+    assert generate_diff(file1_path, file2_path) == expected_result

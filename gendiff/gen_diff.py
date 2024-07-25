1b74ac13e02315310656ed85673c5e17a3558f0f
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-25 21:20:35 +0500

    modify gendiff.py

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -10,13 +10,13 @@ def generate_diff(file1_path, file2_path):
     diff_lines = []
     for key in all_keys:
         if key in data1 and key not in data2:
-            diff_lines.append(f"  - {key}: {data1[key]}")
+            diff_lines.append(f"  - {key}: {(str(data1[key])).lower()}")
         elif key not in data1 and key in data2:
-            diff_lines.append(f"  + {key}: {data2[key]}")
+            diff_lines.append(f"  + {key}: {(str(data2[key])).lower()}")
         elif data1[key] != data2[key]:
-            diff_lines.append(f"  - {key}: {data1[key]}")
-            diff_lines.append(f"  + {key}: {data2[key]}")
+            diff_lines.append(f"  - {key}: {(str(data1[key])).lower()}")
+            diff_lines.append(f"  + {key}: {(str(data2[key])).lower()}")
         else:
-            diff_lines.append(f"    {key}: {data1[key]}")
+            diff_lines.append(f"    {key}: {(str(data1[key])).lower()}")
     result = "{\n" + "\n".join(diff_lines) + "\n}"
     return result

2b058ed12ed6fcbe8228a17917dd356fc356af9c
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-25 23:29:30 +0500

    modify tests/test_gen_diff.py

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -12,4 +12,7 @@ def test_generate_diff():
 }"""
     file1_path = 'tests/fixtures/file1.json'
     file2_path = 'tests/fixtures/file2.json'
+    file1_yml_path = 'tests/fixtures/plain_yml_file1.yaml'
+    file2_yml_path = 'tests/fixtures/plain_yml_file2.yaml'
     assert generate_diff(file1_path, file2_path) == expected_result
+    assert generate_diff(file1_yml_path, file2_yml_path) == expected_result

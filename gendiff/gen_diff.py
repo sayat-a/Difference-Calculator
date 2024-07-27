00d354f470e5b818e7b5a8c1f2835b59ccbb7954
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-27 21:38:56 +0500

    modify gendiff/gen_diff.py

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -1,7 +1,6 @@
 from gendiff.build_diff import build_diff
 from gendiff.parse import load_file
 from gendiff.formatters.stylish import format_stylish
-from gendiff.formatters.plain import format_plain
 
 
 def generate_diff(file1_path, file2_path, formatter='stylish'):
@@ -14,5 +13,3 @@ def generate_diff(file1_path, file2_path, formatter='stylish'):
     diff = build_diff(data1, data2)
     if formatter == 'stylish':
         return format_stylish(diff)
-    elif formatter == 'plain':
-        return format_plain(diff)

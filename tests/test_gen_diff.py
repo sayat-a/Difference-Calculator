0c645c0bf19ed6d19570a56bd87b9212557a0712
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-28 02:57:26 +0500

    modify tests/test_gen_diff.py

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -2,100 +2,18 @@ from gendiff.gen_diff import generate_diff
 
 
 def test_generate_diff_json():
-    expected_result = """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}"""
+    file_path = 'tests/fixtures/diff_json.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        expected_result = file.read()
     file1_path = 'tests/fixtures/nested_file1.json'
     file2_path = 'tests/fixtures/nested_file2.json'
     assert generate_diff(file1_path, file2_path) == expected_result
 
 
 def test_generate_diff_yaml():
-    expected_result = """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}"""
+    file_path = 'tests/fixtures/diff_json.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        expected_result = file.read()
     file1_path = 'tests/fixtures/nested_yml_file1.yaml'
     file2_path = 'tests/fixtures/nested_yml_file2.yaml'
     assert generate_diff(file1_path, file2_path) == expected_result

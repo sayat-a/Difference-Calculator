e9bad5abf7598613e7d2b2253b473768c08ee0d6
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-28 01:15:13 +0500

    modify gendiff/gen_diff.py

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -2,6 +2,7 @@ from gendiff.build_diff import build_diff
 from gendiff.parse import load_file
 from gendiff.formatters.stylish import format_stylish
 from gendiff.formatters.plain import format_plain
+from gendiff.formatters.json import format_json
 
 
 def generate_diff(file1_path, file2_path, formatter='stylish'):
@@ -16,3 +17,5 @@ def generate_diff(file1_path, file2_path, formatter='stylish'):
         return format_stylish(diff)
     elif formatter == 'plain':
         return format_plain(diff)
+    elif formatter == 'json':
+        return format_json(diff)

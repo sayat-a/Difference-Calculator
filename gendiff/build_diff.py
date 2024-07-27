6f73a5262e8313dc0b09ceb8240105c38dc3c030
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-27 21:29:16 +0500

    add gendiff/build_diff.py

---
+++ gendiff/build_diff.py
@@ -0,0 +1,22 @@
+def build_diff(data1, data2):
+    diff = {}
+    all_keys = sorted(set(data1.keys()).union(set(data2.keys())))
+    for key in all_keys:
+        if key not in data2:
+            diff[key] = {'status': 'removed', 'value': data1[key]}
+        elif key not in data1:
+            diff[key] = {'status': 'added', 'value': data2[key]}
+        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+            diff[key] = {
+                'status': 'nested',
+                'children': build_diff(data1[key], data2[key])
+            }
+        elif data1[key] != data2[key]:
+            diff[key] = {
+                'status': 'changed',
+                'old_value': data1[key],
+                'new_value': data2[key]
+            }
+        else:
+            diff[key] = {'status': 'unchanged', 'value': data1[key]}
+    return diff

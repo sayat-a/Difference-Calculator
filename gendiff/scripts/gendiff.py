2b76edf485bfd3e6931bfc3a2dc0c91dbd9abdde
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-27 22:30:35 +0500

    modify gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,10 +8,16 @@ def main():
         description='Compares two configuration file and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        choices=['stylish', 'plain'],
+        help='set format of output (default: stylish)'
+    )
     args = parser.parse_args()
     print(args.first_file, args.second_file)
-    print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(
+        args.first_file, args.second_file, formatter=args.format))
 
 
 if __name__ == '__main__':

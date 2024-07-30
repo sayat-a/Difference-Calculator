943f3cdd91888bb3fd48385fc5cb9766a2054e21
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-30 18:58:50 +0500

    modify tests/test_gen_diff.py

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -20,31 +20,18 @@ def test_generate_diff_yaml():
 
 
 def test_generate_diff():
-    expected_result = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
+    file_path = 'tests/fixtures/expected_result_flat_json.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        expected_result = file.read()
     file1_path = 'tests/fixtures/file1.json'
     file2_path = 'tests/fixtures/file2.json'
     assert generate_diff(file1_path, file2_path) == expected_result
 
 
 def test_generate_diff_json_plain():
-    expected_result = """Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]"""
+    file_path = 'tests/fixtures/expected_result_plain_format.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        expected_result = file.read()
     file1_path = 'tests/fixtures/nested_file1.json'
     file2_path = 'tests/fixtures/nested_file2.json'
     result = generate_diff(file1_path, file2_path, formatter='plain')

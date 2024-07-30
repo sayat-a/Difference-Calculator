f709e0f44af690d6f8d7ee472ce437f958f3ca85
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-30 19:10:49 +0500

    modify tests/test_gen_diff.py

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -1,38 +1,43 @@
+import pytest
 from gendiff.gen_diff import generate_diff
 
 
-def test_generate_diff_json():
-    file_path = 'tests/fixtures/diff_json.txt'
-    with open(file_path, 'r', encoding='utf-8') as file:
+@pytest.mark.parametrize(
+    "file1_path, file2_path, result_path, formatter",
+    [
+        (
+            'tests/fixtures/nested_file1.json',
+            'tests/fixtures/nested_file2.json',
+            'tests/fixtures/diff_json.txt', 'stylish'
+        ),
+        (
+            'tests/fixtures/nested_yml_file1.yaml',
+            'tests/fixtures/nested_yml_file2.yaml',
+            'tests/fixtures/diff_json.txt', 'stylish'
+        ),
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.json',
+            'tests/fixtures/expected_result_flat_json.txt',
+            'stylish'
+        ),
+        (
+            'tests/fixtures/plain_yml_file1.yaml',
+            'tests/fixtures/plain_yml_file2.yaml',
+            'tests/fixtures/expected_result_flat_json.txt',
+            'stylish'
+        ),
+        (
+            'tests/fixtures/nested_file1.json',
+            'tests/fixtures/nested_file2.json',
+            'tests/fixtures/expected_result_plain_format.txt',
+            'plain'
+        )
+    ]
+)
+def test_generate_diff(file1_path, file2_path, result_path, formatter):
+    with open(result_path, 'r', encoding='utf-8') as file:
         expected_result = file.read()
-    file1_path = 'tests/fixtures/nested_file1.json'
-    file2_path = 'tests/fixtures/nested_file2.json'
-    assert generate_diff(file1_path, file2_path) == expected_result
-
-
-def test_generate_diff_yaml():
-    file_path = 'tests/fixtures/diff_json.txt'
-    with open(file_path, 'r', encoding='utf-8') as file:
-        expected_result = file.read()
-    file1_path = 'tests/fixtures/nested_yml_file1.yaml'
-    file2_path = 'tests/fixtures/nested_yml_file2.yaml'
-    assert generate_diff(file1_path, file2_path) == expected_result
-
-
-def test_generate_diff():
-    file_path = 'tests/fixtures/expected_result_flat_json.txt'
-    with open(file_path, 'r', encoding='utf-8') as file:
-        expected_result = file.read()
-    file1_path = 'tests/fixtures/file1.json'
-    file2_path = 'tests/fixtures/file2.json'
-    assert generate_diff(file1_path, file2_path) == expected_result
-
-
-def test_generate_diff_json_plain():
-    file_path = 'tests/fixtures/expected_result_plain_format.txt'
-    with open(file_path, 'r', encoding='utf-8') as file:
-        expected_result = file.read()
-    file1_path = 'tests/fixtures/nested_file1.json'
-    file2_path = 'tests/fixtures/nested_file2.json'
-    result = generate_diff(file1_path, file2_path, formatter='plain')
-    assert result == expected_result
+    assert generate_diff(
+        file1_path, file2_path, formatter=formatter
+    ) == expected_result

ce20b008cf8f69272a58eb47fbfaa6845b710212
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-30 13:47:14 +0500

    modify gendiff/parse.py

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -1,15 +1,19 @@
 import json
 import yaml
+import os
 
 
 def define_extension(file_path):
-    return str(file_path)[-4:]
+    path, extension = os.path.splitext(file_path)
+    return extension
 
 
 def load_file(file_path):
     extension = define_extension(file_path)
     with open(file_path) as file:
-        if extension == 'json':
+        if extension == '.json':
             return json.load(file)
-        elif extension == 'yaml' or extension == '.yml':
+        elif extension == '.yaml' or extension == '.yml':
             return yaml.safe_load(file)
+        else:
+            raise ValueError(f'Unsupported extension {extension}')

6952efb5b886a887681ed321601835dfafc7a99d
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-07-30 13:52:41 +0500

    modify gendiff/gen_diff.py

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -8,10 +8,6 @@ from gendiff.formatters.json import format_json
 def generate_diff(file1_path, file2_path, formatter='stylish'):
     data1 = load_file(file1_path)
     data2 = load_file(file2_path)
-    if data1 is None:
-        data1 = {}
-    if data2 is None:
-        data2 = {}
     diff = build_diff(data1, data2)
     if formatter == 'stylish':
         return format_stylish(diff)
